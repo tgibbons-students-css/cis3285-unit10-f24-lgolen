988458669ffecb4c0c100b5409918e7cd20eb259
Lonnie Golen <lgolen@css.edu>
2024-11-10 22:24:57 -0600

    learning activity done

---
+++ SingleResponsibilityPrinciple/AdjustTradeDataProvider.cs
@@ -0,0 +1,27 @@
+﻿using SingleResponsibilityPrinciple.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class AdjustTradeDataProvider : ITradeDataProvider
+    {
+        ITradeDataProvider tradeProvider;
+
+        public AdjustTradeDataProvider(ITradeDataProvider tradeProvider)
+        {
+            this.tradeProvider = tradeProvider;
+        }
+        public IEnumerable<string> GetTradeData()
+        {
+            var tradeData = tradeProvider.GetTradeData();
+
+            var newTradeData = tradeData.Select(line => line.Replace("GBP", "EUR"));
+
+            return newTradeData;
+        }
+    }
+}
---
+++ SingleResponsibilityPrinciple/URLAsyncProvider.cs
@@ -0,0 +1,34 @@
+﻿using SingleResponsibilityPrinciple.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleResponsibilityPrinciple
+{
+    public class URLAsyncProvider : ITradeDataProvider
+    {
+        ITradeDataProvider tradeProvider;
+
+        public URLAsyncProvider(ITradeDataProvider tradeProvider)
+        {
+            this.tradeProvider = tradeProvider;
+        }
+
+        public Task<IEnumerable<string>> GetTradeAsync()
+        {
+            Task<IEnumerable<string>> task = Task.Run(() => tradeProvider.GetTradeData());
+            return task;
+        }
+        public IEnumerable<string> GetTradeData()
+        {
+            Task<IEnumerable<string>> task = Task.Run(() => GetTradeAsync());
+            task.Wait();
+
+            IEnumerable<string> tradeData = task.Result;
+
+            return tradeData;
+        }
+    }
+}
